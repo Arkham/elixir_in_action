0380187e1109c70040b9bcb4d44be5c9d87c3cc4
Ju Liu <ju.liu@welaika.com>
2016-12-29 14:38:09 +0000

    Add save to database file

--- ch07/todo/lib/todo/server.ex
+++ ch07/todo/lib/todo/server.ex
@@ -1,8 +1,8 @@
 defmodule Todo.Server do
   use GenServer
 
-  def start do
-    GenServer.start(__MODULE__, nil)
+  def start(name) do
+    GenServer.start(__MODULE__, name)
   end
 
   def add_entry(pid, entry) do
@@ -13,15 +13,17 @@ defmodule Todo.Server do
     GenServer.call(pid, {:entries, date})
   end
 
-  def init(nil) do
-    {:ok, Todo.List.new}
+  def init(name) do
+    {:ok, {name, Todo.Database.get(name) || Todo.List.new}}
   end
 
-  def handle_cast({:add_entry, entry}, state) do
-    {:noreply, Todo.List.add_entry(state, entry)}
+  def handle_cast({:add_entry, entry}, {name, todo_list}) do
+    new_todo_list = Todo.List.add_entry(todo_list, entry)
+    Todo.Database.store(name, new_todo_list)
+    {:noreply, {name, new_todo_list}}
   end
 
-  def handle_call({:entries, date}, _from, state) do
-    {:reply, Todo.List.entries(state, date), state}
+  def handle_call({:entries, date}, _from, {_name, todo_list} = state) do
+    {:reply, Todo.List.entries(todo_list, date), state}
   end
 end
