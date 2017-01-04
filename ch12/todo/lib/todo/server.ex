6dfefc902a5a6afeea03cb5630a2fed544e19c61
Ju Liu <ju.liu@welaika.com>
2017-01-04 12:31:32 +0000

    Make todo distributed

---
+++ ch12/todo/lib/todo/server.ex
@@ -0,0 +1,34 @@
+defmodule Todo.Server do
+  use GenServer
+
+  def start_link(name) do
+    IO.puts "Starting Todo.Server for #{name}"
+    GenServer.start_link(__MODULE__, name, name: {:global, {__MODULE__, name}})
+  end
+
+  def whereis(name) do
+    :global.whereis_name({__MODULE__, name})
+  end
+
+  def add_entry(todo_server, entry) do
+    GenServer.cast(todo_server, {:add_entry, entry})
+  end
+
+  def entries(todo_server, date) do
+    GenServer.call(todo_server, {:entries, date})
+  end
+
+  def init(name) do
+    {:ok, {name, Todo.Database.get(name) || Todo.List.new}}
+  end
+
+  def handle_cast({:add_entry, entry}, {name, todo_list}) do
+    new_todo_list = Todo.List.add_entry(todo_list, entry)
+    Todo.Database.store(name, new_todo_list)
+    {:noreply, {name, new_todo_list}}
+  end
+
+  def handle_call({:entries, date}, _from, {_name, todo_list} = state) do
+    {:reply, Todo.List.entries(todo_list, date), state}
+  end
+end
